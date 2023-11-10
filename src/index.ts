7272cd9fa3b7448c63679b6fe66545430e954170
Abraham Sánchez <abrahamsaanchez@gmail.com>
2023-11-10 12:17:24 +0100

    feat: Export the globs.

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import { EslintConfigurationFactory } from './eslint-configuration-factory';
 
 export * from './eslint-configuration-factory';
+export * from './globs';
 export * from './types';
 
 export default EslintConfigurationFactory;
